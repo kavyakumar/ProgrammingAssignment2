ebd665a60645221066d3d5eab533ddffc63fc77a
Kavya Kumar <kavyakumar@users.noreply.github.com>
2014-04-28 08:16:03 +0530

    Final Commit

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
 	inv<- x$getinverse()
 	if(!is.null(inv)) {
-			message("getting cached data")
+			message("Inverse already exists! Getting cached data..")
 			return(inv)
 	}
 	x_new <- x$get()
